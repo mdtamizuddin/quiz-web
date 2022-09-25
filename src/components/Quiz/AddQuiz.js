6ed67c593c006dad3febde8b7bc459fa2e5d0413
mdtamizuddin <tamizrabbi@gmail.com>
2022-09-25 15:22:14 +0600

    Done

--- src/components/Quiz/AddQuiz.js
+++ src/components/Quiz/AddQuiz.js
@@ -1,73 +1,61 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import { useState } from 'react'
-import { useForm } from 'react-hook-form'
 
 const AddQuiz = () => {
-    const [option, setOption] = useState(['Option 1', 'Option 2'])
-    const [quiz, setQuiz] = useState([])
     const [question, setQuestion] = useState('')
-    const [err, setErr] = useState(false)
-    const removeItem = (it) => {
-        const items = option.filter(x => x !== it)
-        setOption(items)
-    }
-    const { register, reset, handleSubmit, } = useForm();
-    const onSubmit = data => {
-        if (question) {
-            setErr(false)
-            setQuiz([...quiz, { question: question, options: data }])
-            reset()
-            setQuestion('')
+
+    const [qCorrect, setQCorrect] = useState('')
+    const [option1, setoption1] = useState('')
+    const [option2, setoption2] = useState('')
+    const [option3, setoption3] = useState('')
+    const [option4, setoption4] = useState('')
+
+
+    const onHandleSubmit = (e) => {
+        e.preventDefault()
+
+
+        if (!question) {
+            alert("Insert A Qustion")
+        }
+        else if (!qCorrect) {
+            alert("Select Correct Ans.")
         }
         else {
-            setErr(true)
+            fetch('https://quiz-server-ouc1-8ryo6cmqm-mdtamiz.vercel.app/quiz', {
+                method: 'post',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({ type: 'quiz', question: question, options: { option1, option2, option3, option4 }, correct: qCorrect })
+            }).then(res => {
+                if (res.status === 200) {
+                    refetch()
+                    setQuestion('')
+                    setQCorrect('')
+                    setoption1('')
+                    setoption2('')
+                    setoption3('')
+                    setoption4('')
+                }
+            })
         }
     }
+    const { isLoading, data: allQuiz, refetch } = useQuery(['repoData'], () =>
+        fetch('https://quiz-server-ouc1-8ryo6cmqm-mdtamiz.vercel.app/quiz',)
+            .then(res => res.json())
+    )
 
+    if (isLoading) return 'Loading...'
     return (
-        <main className='container mx-auto'>
-            <div className='mt-10'>
-                {
-                    quiz.map((q, index) => (
-                        <div key={index} className="mb-2">
-                            <h2
-                                className='text-xl'
-                            >({index + 1}) : {q.question}</h2>
-                            <form className='p-2'>
-                                <div className='flex option mt-2 items-center'>
-                                    <i className="mr-2">1</i>
-                                    <input type="checkbox" className="checkbox" />
-                                    <h2 className='ml-3'>{q.options.option1}</h2>
-
-                                </div>
-                                <div className='flex option mt-2 items-center'>
-                                    <i className="mr-2">2</i>
-                                    <input type="checkbox" className="checkbox" />
-                                    <h2 className='ml-3'>{q.options.option2}</h2>
-                                </div>
-                                <div className='flex option mt-2 items-center'>
-                                    <i className="mr-2">3</i>
-                                    <input type="checkbox" className="checkbox" />
-                                    <h2 className='ml-3'>{q.options.option3}</h2>
-                                </div>
-                                <div className='flex option mt-2 items-center'>
-                                    <i className="mr-2">4</i>
-                                    <input type="checkbox" className="checkbox" />
-                                    <h2 className='ml-3'>{q.options.option4}</h2>
-                                </div>
-
-                            </form>
-
-                        </div>
-                    ))
-                }
-            </div>
+        <main className='container mx-auto px-5'>
 
             <div className='border w-full p-10 mt-20'>
                 <div className="flex justify-between w-full">
                     <h3 className='text-xl'>
                         <i className="fa-solid fa-grip mr-2"></i>
-                        Quistion {quiz.length + 1}
+                        Quistion {allQuiz.length + 1}
                     </h3>
 
                     <select className='px-2 font-bold'>
@@ -82,54 +70,182 @@ const AddQuiz = () => {
                 </div>
                 <div>
                     <input type="text" className='border w-full mt-10 py-1 px-2' placeholder='Your Question' value={question} onChange={(e) => setQuestion(e.target.value)} />
-                    <p className='py-1 text-red-500'>{err && 'Please Insert A Value'}</p>
                     <div className='flex mt-5'>
                         <h5>Media</h5>
                         <select className='w-36 px-2 ml-5'>
-                            <option value="others">Others</option>
+                            <option value="none">None</option>
                             <option value="media">Media</option>
                             <option value="image">image</option>
                         </select>
                     </div>
 
-                    <form onSubmit={handleSubmit(onSubmit)} className='mt-7'>
-                        {
-                            option.map((o, index) => (
-                                <div key={index} className='flex option mt-2 items-center'>
-                                    <i className="fa-solid fa-grip mr-2"></i>
-                                    <input type="checkbox" className="checkbox" />
-                                    <input
-                                        {...register(`option${index + 1}`)}
-                                        type="text"
-                                        placeholder={`Option ${index + 1}`}
-                                        className='px-2 w-40 border ml-5'
-                                        required
-                                    />
-                                    {
-                                        option.length > 1 &&
-                                        <i onClick={() => {
-                                            removeItem(o)
+                    <form onSubmit={onHandleSubmit} className='mt-7'>
 
-                                        }} className="fa-solid delete text-red-500 text-xl ml-4 fa-xmark"></i>
-                                    }
-                                </div>
-                            ))
-                        }
-                        {
-                            option.length <= 3 && <h4
-                                onClick={() => {
-                                    setOption([...option, `Option ${option.length + 1}`])
-                                }}
-                                className='mt-5 px-5 font-bold text-xl'>
-                                <i className="fa-solid  fa-plus"></i>
-                            </h4>
-                        }
+                        <div className='flex option mt-2 items-center'>
+                            <i className="fa-solid fa-grip mr-2"></i>
+                            <input
+                                type="checkbox"
+                                className="checkbox"
+                                checked={qCorrect === option1}
+                                onChange={() => setQCorrect(option1)} />
+                            <input
+                                type="text"
+                                value={option1}
+                                onChange={(e) => setoption1(e.target.value)}
+                                placeholder={`Option 1`}
+                                className='px-2 w-full border ml-5'
+                                required
+                            />
+
+                            <i onClick={() => {
+                                setoption1('')
+                            }} className="fa-solid delete text-red-500 text-xl ml-4 fa-xmark"></i>
+                        </div>
+                        <div className='flex option mt-2 items-center'>
+                            <i className="fa-solid fa-grip mr-2"></i>
+                            <input
+                                type="checkbox"
+                                className="checkbox"
+                                onChange={() => setQCorrect(option2)}
+                                checked={qCorrect === option2}
+                            />
+                            <input
+                                value={option2}
+                                type="text"
+                                onChange={(e) => setoption2(e.target.value)}
+                                placeholder={`Option 2`}
+                                className='px-2 w-full border ml-5'
+                                required
+                            />
+
+                            <i onClick={() => {
+                                setoption2('')
+                            }} className="fa-solid delete text-red-500 text-xl ml-4 fa-xmark"></i>
+                        </div>
+                        <div className='flex option mt-2 items-center'>
+                            <i className="fa-solid fa-grip mr-2"></i>
+                            <input
+                                type="checkbox"
+                                className="checkbox"
+                                checked={qCorrect === option3}
+                                onChange={() => setQCorrect(option3)} />
+                            <input
+                                value={option3}
+                                type="text"
+                                onChange={(e) => setoption3(e.target.value)}
+                                placeholder={`Option 3`}
+                                className='px-2 w-full border ml-5'
+                                required
+                            />
+
+                            <i onClick={() => {
+                                setoption3('')
+                            }} className="fa-solid delete text-red-500 text-xl ml-4 fa-xmark"></i>
+                        </div>
+                        <div className='flex option mt-2 items-center'>
+                            <i className="fa-solid fa-grip mr-2"></i>
+                            <input
+                                type="checkbox"
+                                className="checkbox"
+                                checked={qCorrect === option4}
+                                onChange={() => setQCorrect(option4)} />
+                            <input
+                                value={option4}
+                                type="text"
+                                onChange={(e) => setoption4(e.target.value)}
+                                placeholder={`Option 4`}
+                                className='px-2 w-full border ml-5'
+                                required
+                            />
+
+                            <i onClick={() => {
+                                setoption4('')
+                            }} className="fa-solid delete text-red-500 text-xl ml-4 fa-xmark"></i>
+                        </div>
                         <button className='mx-auto block px-3 mt-5 py-2 text-white bg-blue-900 rounded-xl'>Add Question</button>
                     </form>
                 </div>
             </div>
+
+            <div className='mt-10'>
+                {
+                    allQuiz.map((q, index) => (
+                        <Quiz q={q} index={index} />
+                    ))
+                }
+            </div>
+
         </main>
     )
 }
 
 export default AddQuiz
+
+const Quiz = ({ q, index }) => {
+    const [correct, setCorrect] = useState(false)
+    return (
+        <div className="mb-2">
+            <h2
+                className='text-xl'
+            >({index + 1}) : {q.question}</h2>
+            <div className='p-2'>
+                <div className={`flex option mt-2 items-center`}>
+                    <i className="mr-2">1</i>
+                    <input
+                        onChange={() => setCorrect(q.options.option1)}
+                        type="checkbox" className="checkbox"
+                        checked={q.options.option1 === correct}
+                    />
+                    <h2 className='ml-3'>{q.options.option1}</h2>
+
+                </div>
+                <div className='flex option mt-2 items-center'>
+                    <i className="mr-2">2</i>
+                    <input
+                        onChange={() => setCorrect(q.options.option2)}
+                        type="checkbox" className="checkbox"
+                        checked={q.options.option2 === correct}
+                    />
+                    <h2 className='ml-3'>{q.options.option2}</h2>
+                </div>
+                <div className='flex option mt-2 items-center'>
+                    <i className="mr-2">3</i>
+                    <input
+                        onChange={() => setCorrect(q.options.option3)}
+                        type="checkbox" className="checkbox"
+                        checked={q.options.option3 === correct}
+                    />
+                    <h2 className='ml-3'>{q.options.option3}</h2>
+                </div>
+                <div className='flex option mt-2 items-center'>
+                    <i className="mr-2">4</i>
+                    <input
+                        onChange={() => setCorrect(q.options.option4)}
+                        type="checkbox" className="checkbox"
+                        checked={q.options.option4 === correct}
+                    />
+                    <h2 className='ml-3'>{q.options.option4}</h2>
+                </div>
+                <h2 className='py-2'>
+                    {
+                        correct
+                            ?
+                            correct === q.correct ?
+                                <span className='text-green-500'>
+                                    Selected Ans. Is Correct
+                                </span>
+                                :
+                                <p className='text-red-500'>
+                                    Incorrect Ans. <span className='text-green-500'>
+                                        Correct is : ({q.correct})
+                                    </span>
+                                </p>
+                            :
+                            ""
+                    }
+                </h2>
+            </div>
+
+        </div>
+    )
+}
